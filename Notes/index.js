19d2038b3b146382fc9228950fc85cfa8b2f2d18
MasonHart1 <masonhatr@gmail.com>
2026-04-24 02:58:46 +0000

    ASAASASASASDASASadasasdasdsad

--- Notes/index.js
+++ Notes/index.js
@@ -1,8 +1,13 @@
 emailjs.init("hN5azE5YemTBaGN7m")
 
+let noteId = 1
+
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault()
     emailjs.sendForm("service_c2wxr9s", "template_edbt6jr", this)
     .then(() => alert("Message sent!"))
     .catch((err) => alert("Failed: " + JSON.stringify(err)))
+
+    window.localStorage.setItem("noteId" + noteId, document.getElementsByClassName("message"))
+    noteId +=1;
 })
