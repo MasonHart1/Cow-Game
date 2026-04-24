60afc92d7661623dec0aadcc24de42cf9ac88c87
Mason Hart <maha6a5@student.oaisd.org>
2026-04-23 23:21:49 -0400

    Made it so she can see the notes she sent me

--- Notes/index.js
+++ Notes/index.js
@@ -3,11 +3,13 @@ emailjs.init("hN5azE5YemTBaGN7m")
 let noteId = 1
 
 document.getElementById("contactForm").addEventListener("submit", function (e) {
+    window.localStorage.setItem("noteId" + noteId, document.getElementById("message").value)
     e.preventDefault()
     emailjs.sendForm("service_c2wxr9s", "template_edbt6jr", this)
     .then(() => alert("Message sent!"))
     .catch((err) => alert("Failed: " + JSON.stringify(err)))
 
-    window.localStorage.setItem("noteId" + noteId, document.getElementsByClassName("message"))
     noteId +=1;
+    window.localStorage.setItem("noteId", noteId)
 })
+
